077d135065dee6092ff6704abd3047fcce41a6d7
Mahery Joe <maheryj23@gmail.com>
2022-12-06 12:09:47 +0300

    Dropdown on months

--- lib/screens/expenses.dart
+++ lib/screens/expenses.dart
@@ -8,6 +8,8 @@ class Expense extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String> list = <String>['JANOARY', 'FEBROARY', 'MARTSA', 'APRILY'];
+    String dropdownValue = list.first;
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         child: Container(
@@ -24,13 +26,62 @@ class Expense extends StatelessWidget {
                   end: Alignment.centerRight,
                   stops: [0.1, 1.0],
                   tileMode: TileMode.clamp,
-                  colors: [Colors.blue, Colors.red])),
+                  colors: [Color(0xffc8708a), Colors.red])),
         ),
         onPressed: () {},
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 8.0),
+                child: DropdownButton<String>(
+                  value: dropdownValue,
+                  icon: Image.asset(
+                    "images/down.png",
+                    width: 15,
+                    height: 15,
+                  ),
+                  elevation: 56,
+                  style: const TextStyle(
+                    fontSize: 35.0,
+                    color: Colors.black,
+                    // fontWeight: FontWeight.bold,
+                    fontFamily: 'Bebas Neue',
+                  ),
+                  underline: SizedBox(),
+                  onChanged: (String? value) {},
+                  items: list.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(right: 10.0),
+                child: DropdownButton<String>(
+                  icon: Icon(
+                    Icons.more_vert,
+                    size: 34,
+                  ),
+                  elevation: 56,
+                  underline: SizedBox(),
+                  onChanged: (String? value) {},
+                  items: list.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ],
+          ),
           ReusableCard(
             height: 130.0,
             colour: Colors.orangeAccent,
@@ -41,14 +92,19 @@ class Expense extends StatelessWidget {
               Expanded(
                 child: ReusableCard(
                   height: 90.0,
-                  colour: Colors.blueGrey,
-                  cardChild: Text('Balance'),
+                  colour: Color(0xff918abe),
+                  cardChild: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Balance'),
+                    ],
+                  ),
                 ),
               ),
               Expanded(
                 child: ReusableCard(
                   height: 90.0,
-                  colour: Colors.blueGrey,
+                  colour: Color(0xffff5959),
                   cardChild: Text('Balance'),
                 ),
               ),
